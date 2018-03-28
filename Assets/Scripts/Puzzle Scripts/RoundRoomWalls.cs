3328969b2d5611e2368f001b646a3705bc50014e
OskarMagnusson <oskar.magnusson97@gmail.com>
2018-03-28 15:57:27 +0200

    randomiserar väggar
    nu blir det sköj med väggar

---
+++ Assets/Scripts/Puzzle Scripts/RoundRoomWalls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundRoomWalls : MonoBehaviour {
+
+    [SerializeField]
+    List<GameObject> walls = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        RandomRooms();
+    }
+    
+    private void RandomRooms()
+    {
+		for(int i = 0; i < walls.Count; i++)
+        {
+            if (Random.Range(0, 10) < 5)
+            {
+                walls[i].SetActive(false);
+            }
+	    }
+    }
+
+}
