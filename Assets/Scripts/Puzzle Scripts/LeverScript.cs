a6c645a6bd88e4f24717af9803c43b386e290416
RemiRed <andrelito96@hotmail.com>
2018-03-28 15:54:43 +0200

    -

--- Assets/Scripts/Puzzle Scripts/LeverScript.cs
+++ Assets/Scripts/Puzzle Scripts/LeverScript.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class LeverScript : Interractable {
 
-   
+    Animator anim;
+    bool leverPulled = false;
 
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
     void Pulling()
     {
+        if (leverPulled == false) {
+            
+            leverPulled = true;
+            anim.Play("Pull");
+            Debug.Log("The Lever is now Pulled");
+        }
 
+        
     }
     void LettinGo()
     {
-
+        if (leverPulled =!false)
+        {
+           
+            anim.Play("Return");
+            leverPulled = false;
+        }
     }
-
+ 
 }
