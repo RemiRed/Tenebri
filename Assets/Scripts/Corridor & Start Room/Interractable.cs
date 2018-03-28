1ae73799b82d43101e5b629e9f20edd3d94e7c05
SofiaJT <sofia.joe.te@gmail.com>
2018-03-28 12:10:51 +0200

    Revert "Revert "Revert "Merge branch 'Grafiker' of https://github.com/RemiRed/Tenebri into Grafiker"""
    This reverts commit 508bdd6b1bceb6266ba724ee2bbbd8cca3e9328d.

--- Assets/Scripts/Corridor & Start Room/Interractable.cs
+++ Assets/Scripts/Corridor & Start Room/Interractable.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Interractable : MonoBehaviour
+public class Interractable : NetworkBehaviour
 {
     [SerializeField]
     string interractMethodName, stopInterractMethodName;
