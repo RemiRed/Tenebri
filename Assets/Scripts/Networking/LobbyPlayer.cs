b7ade33d8f3798bb7c4ba140e492196be53c268e
RemiRed <andrelito96@hotmail.com>
2018-04-03 16:37:50 +0200

    Merge branch 'master' into Maze

---
+++ Assets/Scripts/Networking/LobbyPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class LobbyPlayer : NetworkLobbyPlayer
+{
+    [SerializeField]
+    Button readyButton;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        readyButton = GameObject.FindGameObjectWithTag("ReadyButton").GetComponent<Button>();
+        readyButton.onClick.AddListener(Ready);
+    }
+
+    public void Ready()
+    {
+        if (!readyToBegin)
+        {
+            SendReadyToBeginMessage();
+        }
+        else
+        {
+            SendNotReadyToBeginMessage();
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Networking/NetworkingMatch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class NetworkingMatch : NetworkBehaviour
+{
+    public MatchInfoSnapshot matchInfoSnapshot = new MatchInfoSnapshot();
+    public MatchInfo matchInfo = new MatchInfo();
+    [SerializeField]
+    NetworkingLobby networkManager;
+
+    [SerializeField]
+    bool matchList = true;
+    private void Start()
+    {
+        networkManager = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<NetworkingLobby>();
+    }
+    private void Update()
+    {
+        if (matchList)
+        {
+            GetComponentInChildren<Text>().text = matchInfoSnapshot.name;
+        }
+    }
+
+    public void JoinMatch()
+    {
+        networkManager.MatchmakingJoinMatch(matchInfoSnapshot);
+    }
+
+    public void DestroyMatch()
+    {
+        networkManager.MatchmakingDestroyMatch(matchInfo);
+    }
+}
