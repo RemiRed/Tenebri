d12a3463bb2823b6f5c549edf935f0225fcc1ad1
DanielBengtssonST <b_leinad@hotmail.com>
2018-04-04 15:57:30 +0200

    Random Path should be fixed now

--- Assets/Scripts/Puzzle Scripts/RoundDoors.cs
+++ Assets/Scripts/Puzzle Scripts/RoundDoors.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RoundDoors : MonoBehaviour {
 
+	public RoundRoomWalls resetManager;
+
     [SerializeField]
     List<GameObject> WallList = new List<GameObject>(),
 					availableRoomsList = new List<GameObject>(), //room list har alla angränsande rum som antingen är på samma lager eller ett lager in, wall list är väggarna till de rummen.
@@ -21,87 +23,124 @@ public class RoundDoors : MonoBehaviour {
 		}
 	}
 
-	// Use this for initialization
-	void Start () {
-
-	
-	}
-
-    public void FindPath()
+    public bool FindPath()
     {
+		bool _bool = true;
 		if (layer != 0) {
 
 			//Allows for infinite rests for improved debugging
 			availableRoomsList.Clear ();
-			Debug.Log ("Backup list : " + backupRoomList.Count);
 			foreach(GameObject _room in backupRoomList){
 
 				availableRoomsList.Add (_room);
 			}
 				
-			//Improved layout randomization
-			int randomRoomToKill = Random.Range (0, availableRoomsList.Count);
-			Debug.Log (randomRoomToKill + " " + availableRoomsList.Count);
-			availableRoomsList.RemoveAt (randomRoomToKill);
+//			//Improved layout randomization. Maybe? 
+//			int randomRoomToKill = Random.Range (0, availableRoomsList.Count);
+//			availableRoomsList.RemoveAt (randomRoomToKill);
 
+			//Removes previously entered rooms from list of availible rooms
 			bool2 = true;
 
 			List<int> _intList = new List<int> ();
 			for (int i = 0; i < availableRoomsList.Count; i++) {
-				if (availableRoomsList [i].GetComponent<RoundDoors> ().bool2) {
+				if (availableRoomsList [i].GetComponent<RoundDoors> ().bool2 /*|| availableRoomsList[i].GetComponent<RoundDoors>().layer > layer*/){
 					_intList.Add (i);
 				}
 			}
 			for (int i = 0; i < _intList.Count; i++) {
 				availableRoomsList.RemoveAt (_intList [i] - i);
 			}
+				
+			//Checks if path reached a dead end. 
+			if (availableRoomsList.Count == 0) {	//If True: return to origin and try again
 
-			int randomRoomID = Random.Range (0, availableRoomsList.Count);
-
-			//Debug.Log (randomRoomID + " " + availableRoomsList.Count);
-
-			if (availableRoomsList.Count == 0) {
-
-				Debug.LogWarning ("Got stuck in a dead end");
+				//Debug.LogWarning ("Got stuck in a dead end");
 				if (origin != this) {
-					origin.FindPath ();
-					return;
+					_bool = origin.FindPath ();
+					return _bool;
 				} else {
 					Debug.LogWarning ("<! DEAD END GOT STUCK !>");
-					return;
+					return false;
 				}
-			}
 
-			RoundDoors room = availableRoomsList [randomRoomID].GetComponent<RoundDoors> ();
-           
-			Debug.Log (gameObject + " went into " + room.gameObject);
-            
-			foreach (GameObject wall in WallList) {
-				foreach (GameObject walle in room.WallList) {
-					if (wall == walle) {
-						wall.GetComponent<RoundWallDoors> ().OpenSesamy ();
-						break;
+			} else {	//If False: choose a random room to go to
+
+				int randomRoomID = Random.Range (0, availableRoomsList.Count);
+
+				RoundDoors room = availableRoomsList [randomRoomID].GetComponent<RoundDoors> ();
+
+			//	Debug.Log (gameObject + " went into " + room.gameObject);
+
+				//Checks which wall the two rooms shares between them and Removes the wall between the two rooms
+				foreach (GameObject wall in WallList) {
+					foreach (GameObject walle in room.WallList) {
+						if (wall == walle) {
+							wall.GetComponent<RoundWallDoors> ().OpenSesamy ();
+							break;
+						}
 					}
 				}
+					
+				if (room.entered) {	//End path if room previously entered by another path
+
+					Debug.Log ("FOUND ANOTHER BUTTONS PATH");
+
+					bool2 = false;
+					entered = true;
+
+					return _bool;
+
+				} else {	//Continue looking for a path
+
+					room.origin = origin;
+					_bool = room.FindPath ();
+
+					bool2 = false;
+					entered = true;
+				}
 			}
-           
-			Debug.Log (room.gameObject + "Entered = " + room.entered + " and Bool2 = " + bool2);
-
-			if (room.entered) {
-
-				Debug.Log ("FOUND ANOTHER BUTTONS PATH");
-				return;
-			}
-				
-			room.origin = origin;
-			room.FindPath ();
-
-			bool2 = false;
-			entered = true;
 
 		} else {
 			
 			Debug.Log ("FOUND THE CENTER OF THE MAZE");
 		}
+		return _bool;
     }
+
+	public void OpenToRandomRoom(){
+
+		if (entered) {
+
+			return;
+		}
+
+		if (availableRoomsList.Count == 0) {
+
+			Debug.Log ("Screw it");
+
+			availableRoomsList.Clear ();
+			foreach(GameObject _room in backupRoomList){
+
+				availableRoomsList.Add (_room);
+			}
+		}
+
+		int randomRoomID = Random.Range (0, availableRoomsList.Count);
+
+		RoundDoors room = availableRoomsList [randomRoomID].GetComponent<RoundDoors> ();
+
+		Debug.Log ("Open a Door Now from " + gameObject + " to " + room.gameObject);
+
+		//Checks which wall the two rooms shares between them and Removes the wall between the two rooms
+		foreach (GameObject wall in WallList) {
+			foreach (GameObject walle in room.WallList) {
+				if (wall == walle) {
+					wall.GetComponent<RoundWallDoors> ().OpenSesamy ();
+					break;
+				}
+			}
+		}
+
+	}
 }
