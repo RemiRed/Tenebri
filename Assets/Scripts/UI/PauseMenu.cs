6de581f033987fc59fe18cb31f5b1b1a51dc8768
RemiRed <andrelito96@hotmail.com>
2018-04-05 13:51:56 +0200

    shizzy

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pauseMenu, optionsMenu;
+
+    [SerializeField]
+    Slider volumeSlider;
+
+    NetworkingLobby networkingLobby;
+    public CharacterScript character;
+    private void Start()
+    {
+        networkingLobby = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<NetworkingLobby>();
+    }
+
+    public void Resume()
+    {
+        character.ToggleMenu();
+    }
+    public void Options()
+    {
+        pauseMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+    public void MainMenu()
+    {
+        GameObject tempGO = new GameObject();
+        networkingLobby.gameObject.transform.parent = tempGO.transform;
+        networkingLobby.StopHost();
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    //OPTIONS
+    public void Volume()
+    {
+        float volume = volumeSlider.value;
+        AudioListener.volume = volume;
+    }
+    public void Controls()
+    {
+        print("NOT DONE YET");
+    }
+    public void Back()
+    {
+        pauseMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+
+}
