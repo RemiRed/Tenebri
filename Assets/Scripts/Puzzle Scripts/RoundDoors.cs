8f339710a389a826c266e84f6ae8763eda5aaa50
Tanikma <lina.wallius@gmail.com>
2018-04-09 10:01:39 +0200

    börja fixa dekoration med level3

---
+++ Assets/Scripts/Puzzle Scripts/RoundDoors.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundDoors : MonoBehaviour {
+
+	public RoundRoomWalls resetManager;
+
+    [SerializeField]
+    List<GameObject> WallList = new List<GameObject>(),
+					availableRoomsList = new List<GameObject>(), //room list har alla angränsande rum som antingen är på samma lager eller ett lager in, wall list är väggarna till de rummen.
+					backupRoomList = new List<GameObject>();
+    public bool entered = false, bool2 = false;
+    public int layer;
+
+	public RoundDoors origin;
+
+	void Awake(){
+
+		foreach (GameObject _room in availableRoomsList) {
+
+			backupRoomList.Add (_room);
+		}
+	}
+
+    public bool FindPath()
+    {
+		bool _bool = true;
+		if (layer != 0) {
+
+			//Allows for infinite rests for improved debugging
+			availableRoomsList.Clear ();
+			foreach(GameObject _room in backupRoomList){
+
+				availableRoomsList.Add (_room);
+			}
+				
+//			//Improved layout randomization. Maybe? 
+//			int randomRoomToKill = Random.Range (0, availableRoomsList.Count);
+//			availableRoomsList.RemoveAt (randomRoomToKill);
+
+			//Removes previously entered rooms from list of availible rooms
+			bool2 = true;
+
+			List<int> _intList = new List<int> ();
+			for (int i = 0; i < availableRoomsList.Count; i++) {
+				if (availableRoomsList [i].GetComponent<RoundDoors> ().bool2 /*|| availableRoomsList[i].GetComponent<RoundDoors>().layer > layer*/){
+					_intList.Add (i);
+				}
+			}
+			for (int i = 0; i < _intList.Count; i++) {
+				availableRoomsList.RemoveAt (_intList [i] - i);
+			}
+				
+			//Checks if path reached a dead end. 
+			if (availableRoomsList.Count == 0) {	//If True: return to origin and try again
+
+				//Debug.LogWarning ("Got stuck in a dead end");
+				if (origin != this) {
+					_bool = origin.FindPath ();
+					return _bool;
+				} else {
+					Debug.LogWarning ("<! DEAD END GOT STUCK !>");
+					return false;
+				}
+
+			} else {	//If False: choose a random room to go to
+
+				int randomRoomID = Random.Range (0, availableRoomsList.Count);
+
+				RoundDoors room = availableRoomsList [randomRoomID].GetComponent<RoundDoors> ();
+
+			//	Debug.Log (gameObject + " went into " + room.gameObject);
+
+				//Checks which wall the two rooms shares between them and Removes the wall between the two rooms
+				foreach (GameObject wall in WallList) {
+					foreach (GameObject walle in room.WallList) {
+						if (wall == walle) {
+							wall.GetComponent<RoundWallDoors> ().OpenSesamy ();
+							break;
+						}
+					}
+				}
+					
+				if (room.entered) {	//End path if room previously entered by another path
+
+					Debug.Log (origin + "FOUND ANOTHER BUTTONS PATH");
+
+					bool2 = false;
+					entered = true;
+
+					return _bool;
+
+				} else {	//Continue looking for a path
+
+					room.origin = origin;
+					_bool = room.FindPath ();
+
+					bool2 = false;
+					entered = true;
+				}
+			}
+
+		} else {
+			
+			Debug.Log (origin + " FOUND THE CENTER OF THE MAZE");
+		}
+		return _bool;
+    }
+
+	public void OpenToRandomRoom(){
+
+		if (entered) {
+
+			return;
+		}
+		entered = true;
+
+		if (availableRoomsList.Count == 0) {
+
+			Debug.Log ("Screw it");
+
+			availableRoomsList.Clear ();
+			foreach(GameObject _room in backupRoomList){
+
+				availableRoomsList.Add (_room);
+			}
+		}
+
+		int randomRoomID = Random.Range (0, availableRoomsList.Count);
+
+		RoundDoors room = availableRoomsList [randomRoomID].GetComponent<RoundDoors> ();
+
+		Debug.Log ("Open a Door Now from " + gameObject + " to " + room.gameObject);
+
+		//Checks which wall the two rooms shares between them and Removes the wall between the two rooms
+		foreach (GameObject wall in WallList) {
+			foreach (GameObject walle in room.WallList) {
+				if (wall == walle) {
+					wall.GetComponent<RoundWallDoors> ().OpenSesamy ();
+					break;
+				}
+			}
+		}
+		room.availableRoomsList.Remove (gameObject);
+	}
+}
---
+++ Assets/Scripts/Puzzle Scripts/RoundRoomWalls.cs
@@ -0,0 +1,199 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundRoomWalls : MonoBehaviour {
+
+	//Debug stuff
+	public bool _testing;
+	int testNumber = 0;
+
+    [SerializeField]
+    List<GameObject> walls = new List<GameObject>();
+    [SerializeField]
+    List<GameObject> buttons = new List<GameObject>();
+
+	[SerializeField]
+	int numberOfButtons;
+
+	// Use this for initialization
+	void Start () {
+
+		if (!_testing) {
+			
+			RandomSymbols ();
+		}
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+            CloseWalls();
+		
+		if ( _testing || Input.GetKeyDown(KeyCode.G))
+        {
+			testNumber++;
+            RandomSymbols();
+        }
+    }
+    
+    void RandomSymbols()
+    {
+		CloseWalls();
+
+        List<GameObject> tempButtons = new List<GameObject>();
+		foreach (GameObject button in buttons)
+		{
+			tempButtons.Add(button);
+		}
+
+        int tempLayer = 0;
+        bool firstLayer = false;
+        
+		for(int i = 0; i < numberOfButtons; i++)
+        {
+            int randomDude = Random.Range(0, buttons.Count-i);
+
+			if(tempButtons[randomDude] && i == 0)
+            {
+                tempButtons[randomDude].GetComponent<Renderer>().material.color = Color.red;
+				tempButtons [randomDude].GetComponent<RoundDoors> ().origin = tempButtons [randomDude].GetComponent<RoundDoors> ();
+				if (!tempButtons [randomDude].GetComponent<RoundDoors> ().FindPath ()) {
+					Debug.LogError ("RE-RANDOM EVERYTHING! at " + testNumber);
+					_testing = false;
+					testNumber = 0;
+				
+					RandomSymbols ();
+					break;
+				} 
+                tempLayer = tempButtons[randomDude].GetComponent<RoundDoors>().layer;
+                if (tempButtons[randomDude].GetComponent<RoundDoors>().layer == 1)
+                    firstLayer = true;
+                tempButtons.Remove(tempButtons[randomDude]);
+            }
+            else if(firstLayer != true)
+            {
+                List<GameObject> tempButtonslayers = new List<GameObject>();
+                foreach(GameObject newButton in tempButtons)
+                {
+                    if (newButton.GetComponent<RoundDoors>().layer != tempLayer)
+                    {
+                        tempButtonslayers.Add(newButton);
+                    }
+                }
+                int anotherRandomDude = Random.Range(0, tempButtonslayers.Count - 1);
+                if (tempButtonslayers[anotherRandomDude])
+                {
+                    tempButtonslayers[anotherRandomDude].GetComponent<Renderer>().material.color = Color.red;
+                    tempButtonslayers[anotherRandomDude].GetComponent<RoundDoors>().origin = tempButtonslayers[anotherRandomDude].GetComponent<RoundDoors>();
+					if (!tempButtonslayers [anotherRandomDude].GetComponent<RoundDoors> ().FindPath ()) {
+						Debug.LogError ("RE-RANDOM EVERYTHING! at " + testNumber);
+						_testing = false;
+						testNumber = 0;
+
+						RandomSymbols ();
+						break;
+					} 
+                    tempLayer = tempButtonslayers[anotherRandomDude].GetComponent<RoundDoors>().layer;
+                    if (tempButtonslayers[anotherRandomDude].GetComponent<RoundDoors>().layer == 1)
+                        firstLayer = true;
+                    tempButtons.Remove(tempButtonslayers[anotherRandomDude]);
+                }
+            }
+            else
+            {
+                List<GameObject> tempButtonslayers = new List<GameObject>();
+                foreach (GameObject newButton in tempButtons)
+                {
+                    if (newButton.GetComponent<RoundDoors>().layer != tempLayer && newButton.GetComponent<RoundDoors>().layer != 1)
+                    {
+                        tempButtonslayers.Add(newButton);
+                    }
+                }
+                int anotherRandomDude = Random.Range(0, tempButtonslayers.Count - 1);
+                if (tempButtonslayers[anotherRandomDude])
+                {
+                    tempButtonslayers[anotherRandomDude].GetComponent<Renderer>().material.color = Color.red;
+                    tempButtonslayers[anotherRandomDude].GetComponent<RoundDoors>().origin = tempButtonslayers[anotherRandomDude].GetComponent<RoundDoors>();
+					if (!tempButtonslayers [anotherRandomDude].GetComponent<RoundDoors> ().FindPath ()) {
+						Debug.LogError ("RE-RANDOM EVERYTHING! at " + testNumber);
+						_testing = false;
+						testNumber = 0;
+
+						RandomSymbols ();
+						break;
+					} 
+                    tempLayer = tempButtonslayers[anotherRandomDude].GetComponent<RoundDoors>().layer;
+                    tempButtons.Remove(tempButtonslayers[anotherRandomDude]);
+                }
+            }
+        }
+
+		foreach(GameObject _rest in tempButtons){
+		
+			if (!_rest.GetComponent<RoundDoors> ().entered) {
+
+				_rest.GetComponent<RoundDoors> ().origin = _rest.GetComponent<RoundDoors> ();
+				_rest.GetComponent<RoundDoors> ().FindPath ();
+			}
+
+			//_rest.GetComponent<RoundDoors>().OpenToRandomRoom ();
+		}
+    }
+		
+	//Resets Everything to default
+	void CloseWalls()
+	{
+		foreach(GameObject bwa in walls)
+		{
+			bwa.SetActive(true);
+		}
+		NeutralSymbols();
+	}
+
+	//Resets all buttons to default
+    void NeutralSymbols()
+    {
+        foreach(GameObject bwu in buttons)
+        {
+            bwu.GetComponent<Renderer>().material.color = Color.gray;
+			bwu.GetComponent<RoundDoors> ().origin = null;
+			bwu.GetComponent<RoundDoors> ().entered = false;
+			bwu.GetComponent<RoundDoors> ().bool2 = false;
+        }
+    }
+}
+	  
+//	public void RandomSymbols()
+//    {
+//		CloseWalls();
+//
+//        List<GameObject> tempButtons = new List<GameObject>();
+//        foreach (GameObject button in buttons)
+//        {
+//            tempButtons.Add(button);
+//        }
+//		for(int i = 0; i < numberOfButtons; i++)
+//        {
+//            int randomDude = Random.Range(0, buttons.Count-i);
+//
+//			if(tempButtons[randomDude])
+//            {
+//                tempButtons[randomDude].GetComponent<Renderer>().material.color = Color.red;
+//				tempButtons [randomDude].GetComponent<RoundDoors> ().origin = tempButtons [randomDude].GetComponent<RoundDoors> ();
+//				if (!tempButtons [randomDude].GetComponent<RoundDoors> ().FindPath ()) {
+//					Debug.LogError ("RE-RANDOM EVERYTHING! at " + testNumber);
+//					_testing = false;
+//					testNumber = 0;
+//
+//					RandomSymbols ();
+//					break;
+//				} 
+//				tempButtons.Remove (tempButtons [randomDude]);
+//            }
+//        }
+//		foreach(GameObject _rest in tempButtons){
+//
+//			_rest.GetComponent<RoundDoors>().OpenToRandomRoom ();
+//		}
+//    }
