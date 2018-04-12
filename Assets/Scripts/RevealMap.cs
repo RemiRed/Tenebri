533468e9105e15e17aaf055a0fdf96e109f01287
RemiRed <andrelito96@hotmail.com>
2018-04-12 19:43:20 +0200

    -

---
+++ Assets/Scripts/RevealMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RevealMap : NetworkBehaviour {
+
+    [ClientRpc]
+    public void RpcRevealMap() {
+        //Reveal the map
+        print("Lever on clients");
+    }
+
+    [ClientRpc]
+    public void RpcWallRemover()
+    {
+        // Flytta väggen.
+    }
+
+
+
+}
