16b4540fb56e34f92dcce89ba0460a2d078f72a4
RemiRed <andrelito96@hotmail.com>
2018-04-16 00:55:15 +0200

    SumChanges

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField]
+    RoomVariables room;
+
+    int lastTick;
+    // Use this for initialization
+   /* void Start()
+    {
+        transform.localEulerAngles = new Vector3(-6 * room.timerSeconds, 0, 0);
+        lastTick = (int)room.timerSeconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (room.startTimer)
+        {
+            print("I AM MOVING");
+            transform.localEulerAngles = new Vector3(-6 * room.currentTime, 0, 0);
+            if ((int)room.timerSeconds < lastTick)
+            {
+                lastTick = (int)room.timerSeconds;
+                //tick sound here
+    }      
+
+        }
+    }*/
+}
--- Assets/Scripts/Puzzle Scripts/Outdoor Maze/LeverScript.cs
+++ Assets/Scripts/Puzzle Scripts/Outdoor Maze/LeverScript.cs
@@ -2,36 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeverScript : Interractable {
+public class LeverScript : Interractable
+{
 
     Animator anim;
     bool leverPulled = false;
+    bool a_isPulling;
+
 
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        a_isPulling = false;
     }
-    
+
     void Pulling()
     {
-        if (leverPulled == false) {
-            
+        if (leverPulled == false)
+        {
+
+            a_isPulling = true;
+
+            if (a_isPulling == true)
+            {
+                anim.SetBool("isPulling", true);
+            }
+
             leverPulled = true;
-            anim.Play("Pull");
+           // anim.Play("Pull");
             Debug.Log("The Lever is now Pulled");
         }
 
-        
+
     }
     void LettinGo()
     {
-        if (leverPulled =!false)
-        {
-           
-            anim.Play("Return");
-            leverPulled = false;
-        }
+        if (leverPulled)
+            if (leverPulled = !false)
+            {
+                anim.SetBool("isPulling", false);
+                
+                leverPulled = false;
+            }
     }
- 
 }
