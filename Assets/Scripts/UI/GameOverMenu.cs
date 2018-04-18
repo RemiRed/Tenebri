ae5851d8e5ce9a368c618cfee0bafa002e1c1698
Andreas Halldin <iqed@live.se>
2018-04-18 11:20:24 +0200

    Game Over 0.1

---
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverMenu : MonoBehaviour {
+
+    //FIND NETWORK LOBBY
+        //CLOSE CONNECTION
+}
