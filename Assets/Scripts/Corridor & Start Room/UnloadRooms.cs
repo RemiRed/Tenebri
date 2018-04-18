ede7b7c2a0f9608c406a2bb1c2ac63f619d9533d
Andreas Halldin <iqed@live.se>
2018-04-18 23:55:56 +0200

    Laggy?

---
+++ Assets/Scripts/Corridor & Start Room/UnloadRooms.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnloadRooms : MonoBehaviour {
+
+    RoomVariables room;
+    RoomLoader roomLoader;
+
+    private void Start()
+    {
+        roomLoader = GameObject.FindGameObjectWithTag("RoomLoader").GetComponent<RoomLoader>();
+        room = transform.parent.GetComponent<RoomVariables>();
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            Unload();
+        }
+    }
+
+    void Unload()
+    {
+        roomLoader.UnloadAllRoomsExcept(RoomLoader.Room.startRoom);
+        roomLoader.UnloadAlllCorridorsExcept(0);
+    }
+}
--- Assets/Scripts/RoomLoader.cs
+++ Assets/Scripts/RoomLoader.cs
@@ -43,6 +43,7 @@ public class RoomLoader : NetworkBehaviour
         }
     }
     
+    
 
 
     public void UnloadAllRoomsExcept(Room room)
