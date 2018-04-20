12ad2df7cb0f21cafbe9fe4da352d617178da643
Andreas Halldin <iqed@live.se>
2018-04-20 11:27:14 +0200

    Testing

--- Assets/Scripts/Corridor & Start Room/UnloadRooms.cs
+++ Assets/Scripts/Corridor & Start Room/UnloadRooms.cs
@@ -1,19 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class UnloadRooms : MonoBehaviour
+public class UnloadRooms : NetworkBehaviour
 {
     [SerializeField]
     RoomLoader roomLoader;
 
+    [SerializeField]
+    UnloadRooms otherUnloadRooms;
+
+    [SyncVar]
+    public bool entered;
 
     private void OnTriggerEnter(Collider c)
     {
+
         if (c.tag == "Player")
         {
-            Unload();
+            CmdEntered();
+            if (entered && otherUnloadRooms.entered)
+            {
+                Unload();
+            }
         }
+    }
+
+    [Command]
+    void CmdEntered()
+    {
+        entered = true;
     }
 
     void Unload()
