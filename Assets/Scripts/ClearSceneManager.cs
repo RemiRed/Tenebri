a6d6139b477e4a15bf573be1455ef7f86f92c3ba
OskarMagnusson <oskar.magnusson97@gmail.com>
2018-04-20 15:47:38 +0200

    Video

---
+++ Assets/Scripts/ClearSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ClearSceneManager : MonoBehaviour {
+    [SerializeField]
+    GameObject canvas;
+    [SerializeField]
+    VideoPlayer video;
+
+    [SerializeField]
+    float delay;
+    bool videoCheck = false;
+
+    void Start()
+    {
+        StartCoroutine(WaitForVideo());
+    }
+
+    IEnumerator WaitForVideo()
+    {
+        print("Started video");
+        yield return new WaitForSecondsRealtime((float)video.clip.length + delay);
+        print("Ended video");
+        canvas.SetActive(true);
+    }
+
+}
--- Assets/Scripts/ExitPlane.cs
+++ Assets/Scripts/ExitPlane.cs
@@ -2,30 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Video;
 
 public class ExitPlane : MonoBehaviour
 {
-    [SerializeField]
-    VideoPlayer video;
-    bool videoCheck = false;
-
     private void OnTriggerEnter(Collider c)
     {
         if (c.tag == "Player")
         {
-            video.targetCamera = c.gameObject.GetComponentInChildren<Camera>();
-            video.Play();
-            videoCheck = true;
+            SceneManager.LoadScene(2);
         }
     }
-
-    private void Update()
-    {
-        if (!video.isPlaying && videoCheck)
-        {
-            //Byt till Victory Scene
-            print("Victory scene here");
-        }
-    }
+    
 }
