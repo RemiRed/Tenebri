8ce13ca7a1f397af920751ce841c50c51254bfbb
DanielBengtssonST <b_leinad@hotmail.com>
2018-04-26 17:53:07 +0200

    Revert "debugging this sh..."
    This reverts commit 1e5faedb91cf8595941ef622f08c5e16b74ec3ca.

--- Assets/Scripts/Puzzle Scripts/Round Maze/RoundRoomWalls.cs
+++ Assets/Scripts/Puzzle Scripts/Round Maze/RoundRoomWalls.cs
@@ -104,7 +104,6 @@ public class RoundRoomWalls : RoomVariables
         {
 
             curButtonNumber++;
-			Debug.Log (_button);
             _button.tag = "Interractable";
             //Sets first button to be the correct matching button
             if (curButtonNumber <= numberOfPasswordButtons)
