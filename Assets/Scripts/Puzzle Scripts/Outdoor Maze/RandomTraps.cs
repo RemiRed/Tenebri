8591ce14d424b8297b0d736ec55815836491ef20
OskarMagnusson <oskar.magnusson97@gmail.com>
2018-05-11 15:03:25 +0200

    random traps
    En liten prototyp script för att randomisera trapsen

---
+++ Assets/Scripts/Puzzle Scripts/Outdoor Maze/RandomTraps.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTraps : MonoBehaviour {
+
+    [SerializeField]
+    List<GameObject> traps = new List<GameObject>();
+    [SerializeField]
+    int maxTraps, minTraps;
+    
+	// Use this for initialization
+	void Start () {
+        RandomizeTrap();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("g"))
+        {
+            foreach(GameObject gre in traps)
+            {
+                gre.SetActive(true);
+            }
+            RandomizeTrap();
+        }
+	}
+
+    void RandomizeTrap()
+    {
+
+        int howMany = Random.Range(minTraps, maxTraps + 1);
+        List<GameObject> tempTraps;
+        tempTraps = new List<GameObject>(traps);
+        for(int i = 0; i < howMany; i++)
+        {
+            int theOne = Random.Range(0, tempTraps.Count);
+            tempTraps[theOne].SetActive(false);
+            tempTraps.Remove(tempTraps[theOne]);
+        }
+    }
+}
