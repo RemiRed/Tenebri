c69eac4425f980583e28bd42360105413968fb0c
Andreas Halldin <iqed@live.se>
2018-05-14 17:06:28 +0200

    Testing Instaclear
    Hotkeys:
    1 = Color Symbols Clear
    2 = Round Maze Clear
    Tab = Open Corridors

---
+++ Assets/Scripts/TESTING.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTING : MonoBehaviour
+{
+    //TESTING ONLY REMOVE THIS BEFORE RELEASE
+    [SerializeField]
+    RoomVariables colorSymbolsP1, colorSymbolsP2, roundMazeP1, roundMazeP2;
+    [SerializeField]
+    List<Interractable> corridorLevers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Testing REMOVE THIS BEFORE RELEASE!!!!!!!!!!!!
+        if (Input.GetKeyDown(KeyCode.Alpha1)) //ColorSymbolsSolve
+        {
+            colorSymbolsP1.OpenDoorToNextLevel();
+            colorSymbolsP2.OpenDoorToNextLevel();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) //RoundMazeSolve
+        {
+            roundMazeP1.OpenDoorToNextLevel();
+            roundMazeP2.OpenDoorToNextLevel();
+        }
+        if (Input.GetKeyDown(KeyCode.Tab)) //OpenCorridors
+        {
+            foreach (Interractable lever in corridorLevers)
+            {
+                lever.Interract();
+            }
+        }
+    }
+}
