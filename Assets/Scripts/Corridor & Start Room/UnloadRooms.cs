d84d841de80d9d0d4f8093aaa0cc0488ec7d3fa8
Andreas Halldin <iqed@live.se>
2018-05-16 11:24:30 +0200

    Pyush

--- Assets/Scripts/Corridor & Start Room/UnloadRooms.cs
+++ Assets/Scripts/Corridor & Start Room/UnloadRooms.cs
@@ -27,24 +27,26 @@ public class UnloadRooms : NetworkBehaviour
 
     private void OnTriggerEnter(Collider c)
     {
+        if (unloaded)
+        {
+            return;
+        }
         if (c.tag == "Player")
         {
-            if (!check && id == 1)
+            bool tempCheck = true;
+            foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
             {
-                bool tempCheck = true;
-                foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+                if (player.GetComponent<NetworkTransform>().netId.Value < c.GetComponent<NetworkTransform>().netId.Value)
                 {
-                    if (player.GetComponent<NetworkTransform>().netId.Value < c.GetComponent<NetworkTransform>().netId.Value)
-                    {
-                        tempCheck = false;
-                        break;
-                    }
+                    tempCheck = false;
+                    break;
                 }
-                if (tempCheck)
-                {
-                    c.gameObject.transform.position = otherUnloadRooms.gameObject.transform.position;
-                }
-            }          
+            }
+            if (tempCheck)
+            {
+                c.gameObject.transform.position = otherUnloadRooms.gameObject.transform.position;
+
+            }
             playerCmd = c.gameObject.GetComponent<PlayerCommands>();
             playerCmd.CmdStartRoomLanded(id, true);
             StartCoroutine(fadeFromBlack.Fade());
