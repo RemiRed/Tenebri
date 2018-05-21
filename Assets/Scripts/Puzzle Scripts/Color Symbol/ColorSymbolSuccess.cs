f4c45e0df1a0a4795515a0f87a20dad82a648114
Andreas Halldin <iqed@live.se>
2018-05-21 18:51:40 +0200

    Changes

--- Assets/Scripts/Puzzle Scripts/Color Symbol/ColorSymbolSuccess.cs
+++ Assets/Scripts/Puzzle Scripts/Color Symbol/ColorSymbolSuccess.cs
@@ -6,45 +6,41 @@ using UnityEngine.Networking;
 //Manages success and failure for 'ColorSymbol' 
 //(Designed to function from both server and client, but not certain if both are needed)
 
-public class ColorSymbolSuccess : RoomVariables
-{
+public class ColorSymbolSuccess : RoomVariables  {
 
-    ////override method inherited from 'RoomVaribles'
-    public override void CompleteSuccess()
-    {
-        //	if (isServer) {
-        CmdCompleteSuccess();
-        //} else {
-        //	roomPassed = true;
-        //	OpenDoorToNextLevel ();
-        //	roomLoader.LoadRoom (RoomLoader.Room.roundMaze,1);
-        //          roomLoader.LoadRoom(RoomLoader.Room.roundMaze, 2);
-        //      }
-    }
-    [Command]
-    public void CmdCompleteSuccess()
-    {
-        roomPassed = true;
-        OpenDoorToNextLevel();
+	////override method inherited from 'RoomVaribles'
+	public override void CompleteSuccess ()
+	{
+		if (isServer) {
+			RpcCompleteSuccess ();
+		} else {
+			roomPassed = true;
+			OpenDoorToNextLevel ();
+			roomLoader.LoadRoom (RoomLoader.Room.roundMaze,1);
+            roomLoader.LoadRoom(RoomLoader.Room.roundMaze, 2);
+        }
+	}
+    [ClientRpc]
+	public void RpcCompleteSuccess()
+	{
+		roomPassed = true;
+		OpenDoorToNextLevel ();
         roomLoader.LoadRoom(RoomLoader.Room.roundMaze, 1);
         roomLoader.LoadRoom(RoomLoader.Room.roundMaze, 2);
     }
 
-    //override method inherited from 'RoomVaribles'
-    public override void Failure()
-    {
-        //if (isServer)
-        //{
-            CmdFailure();
-        //}
-        //else
-        //{
-        //    Fail();
-        //}
-    }
-    [Command]
-    public void CmdFailure()
-    {
-        Fail();
-    }
+	//override method inherited from 'RoomVaribles'
+	public override void Failure ()
+	{
+		if (isServer) {
+			RpcFailure ();
+		} else {
+			Fail ();
+		}
+	}
+    [ClientRpc]
+	public void RpcFailure()
+	{
+		Fail ();
+	}
 }
