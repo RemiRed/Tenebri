0321d9ce70ec523789ef989328da2b22570da80e
Andreas Halldin <iqed@live.se>
2018-05-21 19:23:35 +0200

    Bara för att vara säker

--- Assets/Scripts/Puzzle Scripts/Color Symbol/ColorSymbolSuccess.cs
+++ Assets/Scripts/Puzzle Scripts/Color Symbol/ColorSymbolSuccess.cs
@@ -12,19 +12,22 @@ public class ColorSymbolSuccess : RoomVariables
     ////override method inherited from 'RoomVaribles'
     public override void CompleteSuccess()
     {
-        //	if (isServer) {
-        //		RpcCompleteSuccess ();
-        //	} else {
-        //roomPassed = true;
-        //OpenDoorToNextLevel ();
-        //roomLoader.LoadRoom (RoomLoader.Room.roundMaze,1);
-        //         roomLoader.LoadRoom(RoomLoader.Room.roundMaze, 2);
-        playercommand = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCommands>();
-        playercommand.CmdColorSymbolCompleteSuccess();
-        // }
+        if (isServer)
+        {
+            RpcCompleteSuccess();
+        }
+        else
+        {
+            roomPassed = true;
+            OpenDoorToNextLevel();
+            roomLoader.LoadRoom(RoomLoader.Room.roundMaze, 1);
+            roomLoader.LoadRoom(RoomLoader.Room.roundMaze, 2);
+            // playercommand = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCommands>();
+            //playercommand.CmdColorSymbolCompleteSuccess();
+        }
     }
-
-    public void CompleteSuccessCmd()
+    [ClientRpc]
+    public void RpcCompleteSuccess()
     {
         roomPassed = true;
         OpenDoorToNextLevel();
