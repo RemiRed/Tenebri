1d1ea8df4bd79b2ebd25faa1001b88842b3586d2
OskarMagnusson <oskar.magnusson97@gmail.com>
2018-05-31 14:15:11 +0200

    en komentar
    det var en riktigt ful kommentar men jag tror den fungerar

--- Assets/Scripts/Puzzle Scripts/Round Maze/RoundRoomMapButtonnumber.cs
+++ Assets/Scripts/Puzzle Scripts/Round Maze/RoundRoomMapButtonnumber.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RoundRoomMapButtonnumber : MonoBehaviour {
-//Questionable script...
 
+// Ett numer som sitter på alla symboler i kartan till round maze som matchar knapparna i labyrinten för att veta vilken knapp som har en symbol.
     public int buttonNumber;
 }
